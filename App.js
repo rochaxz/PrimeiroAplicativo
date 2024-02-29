8f2aeb615b2a022106f8b39f0d1fc4784bb4652b
rochaxz <pedro.santos899@etec.sp.gov.br>
2024-02-29 08:20:58 -0300

    Primeiro Comit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Viu nois em cima do foguete, ela quer o Kyan e quer o Veigh</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
